e6f4d19003ae833df1d15369989cc4341e5eac60
opwe37 <opwe37@naver.com>
2021-07-21 10:26:20 +0900

    7/21 Decorator _ using login_required

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -7,44 +8,49 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+# 로그인 url을 별도로 넘겨줄 수 있음
+@login_required(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            tmp = request.POST.get('hello_world_input')
+    if request.method == 'POST':
+        tmp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = tmp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = tmp
+        new_hello_world.save()
 
-            # HttpResponseRedirect: 리다이렉트 해라
-            # reverse: 장고 앱 내의 router를 이용해서 연결
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # HttpResponseRedirect: 리다이렉트 해라
+        # reverse: 장고 앱 내의 router를 이용해서 연결
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+    hello_world_list = HelloWorld.objects.all()
+    return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
-    model = User()
+    model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
 
+# URLConf의 pk(혹은 slug) 키워드로 넘어온 값을 기준으로
+# model에서 검색 수행 (상속 받은 기본 클래스 내에 구현되어 있을 거라 생각됨)
+# template에 넘겨주는 매개변수인 context에 'target_user'라는 키로 넘김
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -52,33 +58,13 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
