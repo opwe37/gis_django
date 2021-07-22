eef70fe390e933387db42420bfbfd5664f489766
opwe37 <opwe37@naver.com>
2021-07-22 16:01:58 +0900

    7/22 create profilea model

--- practice/settings.py
+++ practice/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-
     'accountapp',
     'profileapp',
 ]
--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,4 +1,4 @@
-urlPatterns = [
+urlpatterns = [
 
 ]
 
