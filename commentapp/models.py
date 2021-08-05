3965bb9d2b596f3c0f6284552a35923159d4f156
opwe37 <opwe37@gmail.com>
2021-08-05 16:17:09 +0900

    08.05 - [Create] Comment Model & Form

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='comment')
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+    content = models.TextField(null=False)
+    create_at = models.DateTimeField(auto_now_add=True)
