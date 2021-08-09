f8226ca5bae9fe0818f4d8c5b60ddabf0208f452
opwe37 <opwe37@gmail.com>
2021-08-09 15:55:32 +0900

    08.09 - [Create] Check Login and Comment Ownership

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
@@ -22,6 +27,11 @@ class CommentCreateView(CreateView):
         return super().form_valid(form)
 
 
+is_owner = [login_required, comment_ownership_required]
+
+
+@method_decorator(is_owner, 'get')
+@method_decorator(is_owner, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
